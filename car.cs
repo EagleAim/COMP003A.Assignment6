a0a630f0ab4544019c97c4653c5710c36e0241d7
Logan King <logan.ramzie.king@gmail.com>
2024-02-25 23:07:07 -0800

    car made

--- car.cs
+++ car.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace COMP003A.Assignment6
+﻿namespace COMP003A.Assignment6
 {
-    internal class car
+    /// <summary>
+    /// Car extends the Vehicle class
+    /// </summary>
+    internal class Car : Vehicle
     {
-    }
+           /***** constructors section *****/
+           // by default, a class has a default constructor that does not take anyparameters
+           // therfore, you do not have to explicitly create one
+           
+        
+           /***** methods section *****/
+           /// <summary>
+           /// Overrides the base Vehicle's definition for GetInfo()
+           /// </summary>
+          public override void GetInfo()
+          {
+              Console.WriteLine($"I'm a {nameof(Car)}");
+              Console.WriteLine("I don't want to disclose my information.");
+          }
+      }
 }
